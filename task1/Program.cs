55af54930977e099fde061837fc094dd46bcc580
Valeriya <leru0411@mail.ru>
2022-07-02 11:12:37 +0300

    add thirth homework

---
+++ task1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 1 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int num = Prompt("Введите пятизначное число: ");
+if (num > 9999 && num < 100000)                 //если число пятизначное
+{
+    int firstpart = num / 1000;                 // первые 2 цифры числа
+    int secondpart = num % 100;                 // последние 2 цифры числа 
+
+    int temp1 = firstpart % 10;                 //вторая цифра 
+    int temp2 = firstpart / 10;                 //получаем первую цифру
+    int temp3 = secondpart % 10;                //последняя цифра
+    int temp4 = secondpart / 10;                //четвертая цифра
+    if (temp1 == temp4 && temp2 == temp3)       //если вторая цифра равна четвертой и первая цифра равна последней
+    {
+        System.Console.WriteLine($"Число {num} является палиндромом");
+    }
+    else System.Console.WriteLine($"Число {num} не палиндром");
+}
+else System.Console.WriteLine("Вы ввели не пятизначное число.");
---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 3 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+int num = Prompt("Введите число: ");
+int count = 1;
+while (count <= num)
+{
+    System.Console.Write(count * count * count + " ");
+    count++;
+}
